0e83611dec5cd3aff8397e8b5bcb9cac7af609bf
claudiaaziz <claudiahannahh@gmail.com>
2024-06-17 10:56:54 -0400

    Initial page frontend done

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      🧚🏻‍♀️
-    </div>
-  );
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    return (
+        <div className='App'>
+            <h1>Chat App</h1>
+            {isLoggedIn ? (
+                <h1>You're Logged In</h1>
+            ) : (
+                <div className='initial-page'>
+                    <div className='inputs'>
+                        <input type='text' placeholder='Name' />
+                        <input type='text' placeholder='Room' />
+                    </div>
+                    <button>Enter Chat</button>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
